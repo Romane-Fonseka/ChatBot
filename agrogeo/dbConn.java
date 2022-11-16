4529838b292ccf6ef0c5ad64ca864e366e6b97e9
Romane-Fonseka <68704051+Romane-Fonseka@users.noreply.github.com>
2022-11-16 10:25:16 +0530

    dbConn

---
+++ agrogeo/dbConn.java
@@ -0,0 +1,46 @@
+package com.example.agrogeo;
+
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Logger;
+
+public class dbConn {
+
+
+        private static final Logger log;
+
+        static {
+            System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$-7s] %5$s %n");
+            log =Logger.getLogger(dbConn.class.getName());
+        }
+
+        public static void main(String[] args) throws Exception {
+            log.info("Loading application properties");
+            Properties properties = new Properties();
+            properties.load(dbConn.class.getClassLoader().getResourceAsStream("application.properties"));
+
+            log.info("Connecting to the database");
+            Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
+            log.info("Database connection test: " + connection.getCatalog());
+
+            log.info("Create database schema");
+            Scanner scanner = new Scanner(dbConn.class.getClassLoader().getResourceAsStream("schema.sql"));
+            Statement statement = connection.createStatement();
+            while (scanner.hasNextLine()) {
+                statement.execute(scanner.nextLine());
+            }
+
+		/*
+		Todo todo = new Todo(1L, "configuration", "congratulations, you have set up JDBC correctly!", true);
+        insertData(todo, connection);
+        todo = readData(connection);
+        todo.setDetails("congratulations, you have updated data!");
+        updateData(todo, connection);
+        deleteData(todo, connection);
+		*/
+
+            log.info("Closing database connection");
+            connection.close();
+
+    }
+}
